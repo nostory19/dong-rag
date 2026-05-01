0eee4e6ecc9ff7554769120ad368880d35136f78
董红豆 <2514184696@qq.com>
2026-05-01 16:05:05 +0800

    first commit

---
+++ src/main/java/com/dong/dongrag/DongRagApplication.java
@@ -0,0 +1,13 @@
+package com.dong.dongrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongRagApplication.class, args);
+    }
+
+}
